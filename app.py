bd918c4d6676e408eceb1b07554757477fa2b54a
garcelomarcia <marcelogarcia91@gmail.com>
2022-12-08 19:39:45 -0600

    first commit with rq

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import json, config
+from utils import entry_order
+from rq import Queue
+from worker import conn
 from flask import Flask, request, jsonify
 from binance.client import Client
 from binance.enums import *
@@ -7,6 +10,7 @@ import pandas as pd
 import time
 
 app = Flask(__name__)
+q = Queue(connection=conn)
 df = pd.read_excel("Book3.xlsx")
 symbols_list = df['Symbol'].to_list()
 symbols_list = [i.split("PERP")[0] for i in symbols_list]
@@ -22,56 +26,6 @@ for key in exchange_info:
             table[key["symbol"]]['Order Decimals'] = int(len(key['filters'][1]['stepSize'])-2)
         else:
             table[key["symbol"]]['Order Decimals'] = 0
-
-
-def entry_order(side, quantity,symbol,price, opp_side, tp,sl):
-    if float(client.futures_position_information(symbol=symbol)[0]['positionAmt']) == 0.0:
-        client.futures_cancel_all_open_orders(symbol=symbol)
-        
-    try:    
-        print(f"sending order: Stop Market Order {side}{quantity}{symbol} @{price}")
-        order = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', quantity=quantity, stopPrice=price)
-        time.sleep(0.5)        
-        order_executed = False
-        while order_executed == False:
-            if float(client.futures_position_information(symbol=symbol)[0]['positionAmt']) != 0.0:            
-                try:
-                    print(f"sending order: Take Profit Order {opp_side}{quantity}{symbol} @{tp}")
-                    tp_order = client.futures_create_order(symbol=symbol, side=opp_side, type='LIMIT', quantity=quantity, price=tp, reduceOnly=True, timeInForce="GTC")
-                    print(f"sending order: Stop Loss {opp_side}{quantity}{symbol} @{sl}")
-                    sl_order = client.futures_create_order(symbol=symbol, side=opp_side, type='STOP_MARKET', quantity=quantity, stopPrice=sl, reduceOnly=True, timeInForce="GTC")
-                except BinanceAPIException as err:
-                    if str(err.message) == "Order would immediately trigger.":
-                        print(f"Exiting trade at Market Price")
-                        client.futures_create_order(symbol=symbol, side=opp_side, type='MARKET', quantity=quantity, reduceOnly=True)
-                        client.futures_cancel_all_open_orders(symbol=symbol)
-                    return False
-                order_executed = True
-                break
-            else:
-                order_executed = False
-    except BinanceAPIException as e:
-        
-        # print("an exception occured - {}".format(e))    
-        if not client.futures_get_open_orders(symbol=symbol) and float(client.futures_position_information(symbol=symbol)[0]['positionAmt']) == 0.0 and str(e.message) == "Order would immediately trigger.":                        
-            print("sending order at market price")
-            order = client.futures_create_order(symbol=symbol, side=side, type='MARKET', quantity=quantity)
-            time.sleep(0.5)
-            try:
-                print(f"sending order: Take Profit Order {opp_side}{quantity}{symbol} @{tp}")
-                tp_order = client.futures_create_order(symbol=symbol, side=opp_side, type='LIMIT', quantity=quantity, price=tp, reduceOnly=True, timeInForce="GTC")
-                print(f"sending order: Stop Loss {opp_side}{quantity}{symbol} @{sl}")
-                sl_order = client.futures_create_order(symbol=symbol, side=opp_side, type='STOP_MARKET', quantity=quantity, stopPrice=sl, reduceOnly=True, timeInForce="GTC")
-            except BinanceAPIException as err:
-                if str(err.message) == "Order would immediately trigger.":
-                    print(f"Exiting trade at Market Price")
-                    client.futures_create_order(symbol=symbol, side=opp_side, type='MARKET', quantity=quantity, reduceOnly=True)
-                    client.futures_cancel_all_open_orders(symbol=symbol)
-                return False
-                
-
-        
-    return order,tp_order,sl_order
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
@@ -101,7 +55,7 @@ def webhook():
     else:
         opp_side = "BUY"
 
-    new_order = entry_order(side, quantity,symbol,price, opp_side, tp,sl)
+    new_order = q.enqueue(entry_order,args=(side, quantity,symbol,price, opp_side, tp,sl), job_timeout=3600)
     if new_order:
         return {
             "code": "success",
